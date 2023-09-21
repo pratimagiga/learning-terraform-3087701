0d2a9be4be2601273e42608e0a169f7d1a479a3a
pratimagiga <126730769+pratimagiga@users.noreply.github.com>
2023-09-20 18:09:24 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,11 +17,13 @@ data "aws_ami" "app_ami" {
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
-  name = "dev"
+  name = "my-vpc"
   cidr = "10.0.0.0/16"
 
-  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
+  azs             = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_vpn_gateway = true
 
   tags = {
     Terraform = "true"
