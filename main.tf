642fc0493951e084e352ff64648905bb4fd6e49e
pratimagiga <126730769+pratimagiga@users.noreply.github.com>
2023-09-21 11:04:34 -0700

    autoscaling

--- main.tf
+++ main.tf
@@ -29,15 +29,20 @@ module "blog_vpc" {
   }
 }
 
-resource "aws_instance" "blog" {
-  ami                    = data.aws_ami.app_ami.id
-  instance_type          = var.instance_type
-  subnet_id              = module.blog_vpc.public_subnets[0]
-  vpc_security_group_ids = [module.blog_sg.security_group_id]
+module "blog_autoscaling" {
+  source  = "terraform-aws-modules/autoscaling/aws"
+  version = "6.10.0"
 
-  tags = {
-    Name = "Learning Terraform"
-  }
+  name = "blog"
+
+  min_size            = 1
+  max_size            = 2
+  vpc_zone_identifier = module.blog_vpc.public_subnets
+  target_group_arns   = module.blog_alb.target_group_arns
+  security_groups     = [module.blog_sg.security_group_id]
+
+  instance_type = var.instance_type
+  image_id      = data.aws_ami.app_ami.id
 }
 
 module "blog_alb" {
@@ -74,7 +79,6 @@ module "blog_alb" {
     Environment = "dev"
   }
 }
-
 
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
