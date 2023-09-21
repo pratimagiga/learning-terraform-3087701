a1ebdc1b4e63ede465181f98358ada2724d603fd
pratimagiga <126730769+pratimagiga@users.noreply.github.com>
2023-09-20 18:18:09 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,16 +14,14 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
-module "vpc" {
+module "blog_vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
-  name = "my-vpc"
+  name = "dev"
   cidr = "10.0.0.0/16"
 
   azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
-
-  enable_vpn_gateway = true
 
   tags = {
     Terraform = "true"
