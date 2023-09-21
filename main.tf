50f0f3698ab34dab3cced9007d17dfbcc433ddb8
pratimagiga <126730769+pratimagiga@users.noreply.github.com>
2023-09-20 18:28:50 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -30,10 +30,10 @@ module "blog_vpc" {
 }
 
 resource "aws_instance" "blog" {
-  ami           = data.aws_ami.app_ami.id
-  instance_type = var.instance_type
-
-  vpc_security_group_ids = [module.blog_sg.security_group_id]
+  ami                    = data.aws_ami.app_ami.id
+  instance_type          = var.instance_type
+  subnet_id              = module.blog_vpc.public_subnets[0]
+  vpc_security_group_ids = [module.blog_sg.security_group_id] 
 
   tags = {
     Name = "Learning Terraform"
@@ -43,13 +43,11 @@ resource "aws_instance" "blog" {
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "5.1.0"
-  name = "blog"
 
-  vpc_id = module.vpc.public_subnets[0]
-
+  vpc_id              = module.blog_vpc.vpc_id
+  name                = "blog"
   ingress_rules       = ["http-80-tcp","https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"] 
-
-  egress_rules       = ["all-all"]
-  egress_cidr_blocks = ["0.0.0.0/0"] 
+  egress_rules        = ["all-all"]
+  egress_cidr_blocks  = ["0.0.0.0/0"] 
 }
