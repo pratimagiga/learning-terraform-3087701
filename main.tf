684a88daa2c43d6f2f96958b24c172f022514998
pratimagiga <126730769+pratimagiga@users.noreply.github.com>
2023-09-20 22:05:24 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,21 +14,6 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
-module "blog_vpc" {
-  source = "terraform-aws-modules/vpc/aws"
-
-  name = "dev"
-  cidr = "10.0.0.0/16"
-
-  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
-
-  tags = {
-    Terraform = "true"
-    Environment = "dev"
-  }
-}
-
 resource "aws_instance" "blog" {
   ami                    = data.aws_ami.app_ami.id
   instance_type          = var.instance_type
@@ -40,14 +25,4 @@ resource "aws_instance" "blog" {
   }
 }
 
-module "blog_sg" {
-  source  = "terraform-aws-modules/security-group/aws"
-  version = "5.1.0"
 
-  vpc_id              = module.blog_vpc.vpc_id
-  name                = "blog"
-  ingress_rules       = ["http-80-tcp","https-443-tcp"]
-  ingress_cidr_blocks = ["0.0.0.0/0"] 
-  egress_rules        = ["all-all"]
-  egress_cidr_blocks  = ["0.0.0.0/0"] 
-}
