58ca871984d7f65c9707f0f84a352e965b46d6a7
pratimagiga <126730769+pratimagiga@users.noreply.github.com>
2023-09-21 10:05:32 -0700

    Added blog_sg

--- main.tf
+++ main.tf
@@ -14,13 +14,34 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
+data "aws_vpc" "default" {
+  default = true
+}
+
 resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
+
+  vpc_security_group_ids = [module.blog_sg.security_group_id]
 
   tags = {
     Name = "Learning Terraform"
   }
 }
+
+module "blog_sg" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "5.1.0"
+  name    = "blog"
+
+  vpc_id              = data.aws_vpc.default.id
+
+  ingress_rules       = ["http-80-tcp","http-8080-tcp"] 
+  ingress_cidr_blocks = ["0.0.0.0/0"] 
+
+  egress_rules       = ["all-all"] 
+  egress_cidr_blocks = ["0.0.0.0/0"] 
+}
+
 
 
