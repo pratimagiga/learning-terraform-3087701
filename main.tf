2e7fd84f49de07caed7ad7fe86e570953d4d2e8c
pratimagiga <126730769+pratimagiga@users.noreply.github.com>
2023-09-20 22:21:28 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,14 +14,12 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
-resource "aws_instance" "blog" {
-  ami                    = data.aws_ami.app_ami.id
-  instance_type          = var.instance_type
-  subnet_id              = module.blog_vpc.public_subnets[0]
-  vpc_security_group_ids = [module.blog_sg.security_group_id] 
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.app_ami.id
+  instance_type = "t3.nano"
 
   tags = {
-    Name = "Learning Terraform"
+    Name = "HelloWorld"
   }
 }
 
