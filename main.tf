26b1109bd9d9d2fe73c6569a60b4353c3fb97396
pratimagiga <126730769+pratimagiga@users.noreply.github.com>
2023-09-20 18:13:26 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -20,7 +20,7 @@ module "vpc" {
   name = "my-vpc"
   cidr = "10.0.0.0/16"
 
-  azs             = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
+  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
   enable_vpn_gateway = true
